638c0db8ed338e4cd3f4239a8b0874eb6fbf072f
Blake Julander <13lakejulander@gmail.com>
2021-05-04 15:48:20 -0700

    Added index, show to Task controller. Updated front end files to match models

--- mhm-backend/app/controllers/tasks_controller.rb
+++ mhm-backend/app/controllers/tasks_controller.rb
@@ -3,4 +3,20 @@ class TasksController < ApplicationController
     def index
         render json: Task.all
     end
+
+    def show
+        task = Task.find_by(id: params[:id])
+        render json: task
+    end
+
+    def create
+        task = Task.new(task_params)
+    end
+
+
+    private
+
+    def task_params
+        params.require(:task).permit(:category_type, :name, :content, :value, :complete, :day_id, :category_id)
+    end
 end
